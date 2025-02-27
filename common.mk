350ef53a6ea2ebd5d5dd40afcb2d7468af2ad31b
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    gs201: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -209,6 +209,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
 
 ifneq ($(TARGET_IS_TABLET),true)
+
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 PRODUCT_CHARACTERISTICS := nosdcard
 else
 PRODUCT_CHARACTERISTICS := nosdcard,tablet
